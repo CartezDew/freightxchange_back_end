7a3ed2516ecabc79a7d98524d8c0252cc164daee
Bri Pintens <btpintens@gmail.com>
2025-05-09 16:15:14 -0400

    initial urls complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,16 @@
-# main_app/urls.py
 from django.urls import path
-from . import views
+from .views import Landing, CreateUserView, LoginView, LogoutView, ProfileView, LoadList, OfferView, CreateLoad, LoadDetail, EditLoad, DeleteLoad
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path ('', Landing.as_view(), name='landing'),
+    path ('/register', CreateUserView.as_view(), name='register'),
+    path ('/login', LoginView.as_view(), name='login'),
+    path ('/logout', LogoutView.as_view(), name='logout'),
+    path ('/profile/:Id', ProfileView.as_view(), name='profile'),
+    path ('/loads', LoadList.as_view(), name='loads'),
+    path ('/offer/:offerId', OfferView.as_view(), name='offers'),
+    path ('/loads/new', CreateLoad.as_view(), name='create-load'),
+    path ('/loads/:loadID/', LoadDetail.as_view(), name='load-detail'),
+    path ('/loads/:loadID/edit', EditLoad.as_view(), name='edit-load'),
+    path ('/loads/:loadID/delete', DeleteLoad.as_view(), name='delete-load'),
 ]
