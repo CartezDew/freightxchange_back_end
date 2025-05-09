2f6327697afc4704020a103f36933ef2311c8b8e
Bri Pintens <btpintens@gmail.com>
2025-05-09 16:40:56 -0400

    began work on views.py

---
+++ main_app/views.py
@@ -0,0 +1,53 @@
+from rest_framework import generics, status, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+
+class Landing(APIView):
+    def get(self, request):
+        content = {'message': 'Welcome to FreightXchange api home route!'}
+        return Response(content)
+    
+#User Registration 
+
+        
+        
+#User Login
+class LoginView(APIView): 
+    permission_classes = [permissions.AllowAny]
+    
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user: 
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'user': UserSerializer(user).data
+            })
+        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+class VerifyUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get(self, request):
+        user = User.objects.get(username=request.user)
+        refresh = RefreshToken.for_user(request.user)
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+            'user': UserSerializer(user).data
+        })
+        
+class LoadList(generics.ListCreateAPIView):
+    serializer_class = LoadSerializer
+    permissions_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+    
