0622c08c0d402eb0bde8d092874373823764102b
Bri Pintens <btpintens@gmail.com>
2025-05-10 11:59:31 -0400

    models complete

--- main_app/views.py
+++ main_app/views.py
@@ -17,37 +17,72 @@ class Landing(APIView):
         
         
 #User Login
-class LoginView(APIView): 
-    permission_classes = [permissions.AllowAny]
+# class LoginView(APIView): 
+#     permission_classes = [permissions.AllowAny]
     
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username, password=password)
-        if user: 
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-                'user': UserSerializer(user).data
-            })
-        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+#     def post(self, request):
+#         username = request.data.get('username')
+#         password = request.data.get('password')
+#         user = authenticate(username=username, password=password)
+#         if user: 
+#             refresh = RefreshToken.for_user(user)
+#             return Response({
+#                 'refresh': str(refresh),
+#                 'access': str(refresh.access_token),
+#                 'user': UserSerializer(user).data
+#             })
+#         return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
-class VerifyUserView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+# class VerifyUserView(APIView):
+#     permission_classes = [permissions.IsAuthenticated]
     
-    def get(self, request):
-        user = User.objects.get(username=request.user)
-        refresh = RefreshToken.for_user(request.user)
-        return Response({
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-            'user': UserSerializer(user).data
-        })
+#     def get(self, request):
+#         user = User.objects.get(username=request.user)
+#         refresh = RefreshToken.for_user(request.user)
+#         return Response({
+#             'refresh': str(refresh),
+#             'access': str(refresh.access_token),
+#             'user': UserSerializer(user).data
+#         })
         
-class LoadList(generics.ListCreateAPIView):
-    serializer_class = LoadSerializer
-    permissions_classes = [permissions.IsAuthenticated]
+# class LoadList(generics.ListCreateAPIView):
+#     serializer_class = LoadSerializer
+#     permissions_classes = [permissions.IsAuthenticated]
     
-    def get_queryset(self):
+#     def get_queryset(self):
+#         user = self.request.user
+#         return Load.objects.filter(user=user)
+    
+# class LoadDetail(generics.RetrieveUpddateDestroyAPIView):
+#     serializer_class = LoadSerializer 
+#     lookup_field = 'id'
+    
+#     def get_queryset(self):
+#         user = self.request.user
+#         return Load.objects.filter(user=user)
+    
+#     def retrieve(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         serializer = self.get_serializer(instance)
+        
+#      # bid or broker or carrier, one for each?  bid_not_associated = Bid.objects.exclude(id_in=intance.bids.all())
+#      # bid_serializer = BidSerializer(bids_not_associated, many=True)
+     
+#      return Response({
+#          'load': serializer.data,
+#          'bids_not_associated': bids_serializer.data
+#      })
+        
+#     def perform_update(self, serializer):
+#         load = self.get_object()
+#         if load.user != self.request.user: 
+#             raise PermissionDenied({"message": "You do not have permission to edit this Load."})
+#         serializer.save()
+        
+#     def perform_destory(self, instance):
+#         if instance.user != self.request.user: 
+#             raise PermissionDenied({"message": "You do not have permission to delete this Load."})
+#         instance.delete()
+        
+# class 
     
